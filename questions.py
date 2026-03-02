0644f2f8bc4ceecd8f3609b7436b9f272d719458
Mythryv <119165516+Mythryv@users.noreply.github.com>
2026-03-02 19:57:42 +0530

    Update questions.py

--- questions.py
+++ questions.py
@@ -1,1 +1,22 @@
-
+questions = [
+    {
+        "question": "What is the capital of India?",
+        "options": ["A. Delhi", "B. Mumbai", "C. Chennai", "D. Kolkata"],
+        "answer": "A"
+    },
+    {
+        "question": "Which language is used for web development?",
+        "options": ["A. Python", "B. HTML", "C. C++", "D. Java"],
+        "answer": "B"
+    },
+    {
+        "question": "What is 10 + 5?",
+        "options": ["A. 12", "B. 13", "C. 15", "D. 20"],
+        "answer": "C"
+    },
+    {
+        "question": "Which keyword is used to define a function in Python?",
+        "options": ["A. function", "B. define", "C. def", "D. fun"],
+        "answer": "C"
+    }
+]
